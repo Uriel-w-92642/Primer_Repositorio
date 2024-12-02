332c7a5434e599fa7bd9b176a1024807fc6a0845
Uriel Vargas Hernández <penanieto968@gmail.com>
2024-12-02 09:51:15 -0600

    Add files via upload

---
+++ P00Presentación.php
@@ -0,0 +1,18 @@
+<?php
+/* CBTIS 89 
+P35Array16.php  
+Vargas Hernandez Uriel
+3°A Programación Matutino
+*/
+echo 'Nombre: Uriel Vargas Hernandez';
+echo '<br>';
+echo '<br>';
+echo 'Carrera y Escuela: CBTIS 89-Programación 3°A';
+echo '<br>';
+echo '<br>';
+echo 'País: México';
+echo '<br>';
+echo '<br>';
+echo 'Cidudad: Durango';
+echo '<br>';
+echo '<br>';
